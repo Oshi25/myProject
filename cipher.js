b8468888628281af19bc73f00c32a500f9ca8505
Oshi25 <malysheva_olya25@mail.ru>
2020-05-05 21:41:41 +0700

    Задание 2. Добавлен шифр Веженера и возможность выбора между шифрами

--- cipher.js
+++ cipher.js
@@ -1,6 +1,19 @@
 let alphabet = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'];
 
-function encrypt() {
+function encript() {
+	let selectValue = document.getElementById('cipher-select').value;
+	
+	switch (selectValue) {
+		case '1':
+			caesar();
+			break;
+		case '2':
+			vigenere();
+			break;
+	}
+}
+
+function caesar() {
 	let sourceText = document.getElementById('source-text').value;
 	let cipherText = '';
 	
@@ -16,5 +29,28 @@ function encrypt() {
 		
 		cipherText += alphabet[letterIndex + 1];
 	}
+	document.getElementById('cipher-text').textContent = cipherText;
+}
+
+function vigenere() {
+	let sourceText = document.getElementById('source-text').value;
+	let key = 'хрюк';
+	
+	let cipherText = '';
+	
+	key = key.repeat(Math.ceil(sourceText.length / key.length));
+	key = key.substr(0, sourceText.length);
+	
+	for (let i = 0; i < sourceText.length; i++) {
+		let sourceLetterIndex = alphabet.indexOf(sourceText[i]) + 1;
+		let keyLetterIndex = alphabet.indexOf(key[i]) + 1;
+		
+		let letterIndex = sourceLetterIndex + keyLetterIndex;
+		if (letterIndex > 33) {
+			letterIndex -= 33;
+		}
+		cipherText += alphabet[letterIndex - 1];
+	}
+	
 	document.getElementById('cipher-text').textContent = cipherText;
 }
